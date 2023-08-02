5cac666ef53d336796f31d5672cae00a50c55973
KushagraKatiha <kushagrakatiha123@gmail.com>
2023-08-03 00:20:06 +0530

    CRUD app completed

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -6,7 +6,7 @@ exports.homeResponse=(req, res)=>{
 
 // CREATE USER USING USER MODEL
 
-exports.createuser= async (req, res)=>{
+exports.createuser = async (req, res)=>{
     
     try {
         const {name, email} = req.body
@@ -40,4 +40,60 @@ exports.createuser= async (req, res)=>{
         })
     }
 
+}
+
+// GET ALL USERS
+
+exports.getuser = async (req, res)=>{
+    try {
+       const users =await userModel.find({})
+
+       res.status(200).json({
+        success: true,
+        users
+       })
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({
+            success: false, 
+            message: error.message
+        })
+    }
+}
+
+// DELETE USER (getting id by URL)
+
+exports.deleteuser = async (req, res)=>{
+    try {
+        const userId = req.params.id
+        const userToBeDel = await userModel.findById(userId)
+        if(!userToBeDel){
+            throw new Error('User does not exists')
+        }
+        const user = await userModel.deleteOne(userToBeDel)
+        res.status(200).json({
+            success: true,
+            message: 'User deleted successfully'
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// UPDATE USER
+
+exports.updateuser = async (req, res)=>{
+    try {
+        userModel.findByIdAndUpdate(req.params.id, req.body)
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({
+            success: false, 
+            message: error.message
+        })
+    }
 }
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const {homeResponse, createuser} = require('../controllers/userControler.js')
+const {homeResponse, createuser, getuser, deleteuser} = require('../controllers/userControler.js')
 const appRoutes = express.Router()
+
 
 appRoutes.get('/',homeResponse)
 appRoutes.post('/createuser',createuser)
+appRoutes.get('/getuser',getuser)
+appRoutes.delete('/deleteuser/:id',deleteuser)
 
 module.exports = appRoutes
